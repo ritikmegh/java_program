fe6f4d0b8891fd7496a04a4e774a932e26115e03
Ritik  meghwanshi <163230462+ritikmegh@users.noreply.github.com>
2024-10-16 13:43:15 +0530

    Add files via upload

---
+++ codechef question3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    int A=sc.nextInt();
+		    int B=sc.nextInt();
+		    	int sum=A%B;
+		    			System.out.println(sum);
+		
+		}
+		
+			
+		   
+		
+
+	}
+}
